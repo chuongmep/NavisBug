c5782100d05f5d3053ca50f5120bef5cbfae4877
chuongmep <31106432+chuongmep@users.noreply.github.com>
2022-11-10 20:54:46 +0800

    share bug navis

---
+++ NavisBug/TestBug.cs
@@ -0,0 +1,39 @@
+﻿using System.Runtime.InteropServices;
+using System.Windows;
+using Autodesk.Navisworks.Api.Plugins;
+using Speckle.Core.Credentials;
+using Speckle.Core.Models;
+
+namespace NavisBug
+{
+    [Plugin("HelloWorld", "ChuongMep",DisplayName = "HelloWorld", ToolTip = "HelloWorld Speckle")]
+    public class TestBug : AddInPlugin
+    {
+        public override int Execute(params string[] parameters)
+        {
+            try
+            {
+                RunProcess();
+                MessageBox.Show("Done");
+                return 0;
+            }
+            catch (Exception e)
+            {
+                throw new ArgumentException(e.ToString());
+            }
+        }
+        async void RunProcess()
+        {
+            string streamId = "<fill id here>";
+            // SQLitePCL.Batteries.Init();
+            // define the base object
+            List<string> datas = new List<string>() {"1", "2", "3"};
+            var commitObject = new Base();
+            commitObject["datas"] = datas;
+            commitObject.totalChildrenCount = datas.Count;
+            Account defaultAccount = AccountManager.GetDefaultAccount();
+            MessageBox.Show(defaultAccount.userInfo.name);
+            await Task.FromResult(streamId);
+        }
+    }
+}
